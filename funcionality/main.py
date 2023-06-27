afc39da9667a781c4c31d9bc4f7be169ef80f6a4
szjankowski <szymonchesstry@gmail.com>
2023-06-27 23:02:09 +0200

    feat: add menu option, and facade class

--- funcionality/main.py
+++ funcionality/main.py
@@ -1,4 +1,3 @@
-# from typing import *
 import string
 
 LETTERS_LOWERCASE = list(string.ascii_lowercase)
@@ -9,7 +8,7 @@ class CaesarMethods:
     def __init__(self):
         pass
 
-    def encrypt(self, text: str, shift: int):
+    def encrypt(self, text: str, shift: int) -> str:
         new_text = ""
 
         for char in text:
@@ -19,30 +18,58 @@ class CaesarMethods:
                 if new_index >= 26:
                     new_index = new_index - 26
                 new_text += LETTERS_LOWERCASE[new_index]
-            else:
+            elif char in LETTERS_UPPERCASE:
                 index = LETTERS_UPPERCASE.index(char)
                 new_index = index + shift
                 if new_index >= 26:
                     new_index = new_index - 26
                 new_text += LETTERS_UPPERCASE[new_index]
-
-        return new_text
-
-    def decrypt(self, text: str, shift: int):
-        new_text = ""
-
-        for char in text:
-            if char in LETTERS_LOWERCASE:
-                index = LETTERS_LOWERCASE.index(char)
-                new_index = index - shift
-                if new_index >= 26:
-                    new_index = new_index - 26
-                new_text += LETTERS_LOWERCASE[new_index]
             else:
-                index = LETTERS_UPPERCASE.index(char)
-                new_index = index - shift
-                if new_index >= 26:
-                    new_index = new_index - 26
-                new_text += LETTERS_UPPERCASE[new_index]
+                new_text += char
 
         return new_text
+
+    def decrypt(self, text: str, shift: int) -> str:
+        return self.encrypt(text, -shift)
+
+
+class CaesarCipherFacade:
+    def __init__(self):
+        self.cipher = CaesarMethods()
+
+    def encrypt_text(self, text: str, shift: int):
+        return self.cipher.encrypt(text, shift)
+
+    def decrypt_text(self, text: str, shift: int):
+        return self.cipher.decrypt(text, shift)
+
+
+def display_menu():
+    print("1. Encrypt text")
+    print("2. Decrypt text")
+
+
+def get_choice():
+    choice = input("Choose option by typing corresponding number: ")
+    return choice
+
+
+def main():
+    cipher = CaesarCipherFacade()
+    display_menu()
+    choice = get_choice()
+
+    if choice == "1":
+        text = input("Enter your text: ")
+        shift = int(input("Enter number of shift: "))
+        print(f"Your encrypted text is: {cipher.encrypt_text(text, shift)}")
+    elif choice == "2":
+        text = input("Enter your text: ")
+        shift = int(input("Enter number of shift: "))
+        print(f"Your decrypted text is: {cipher.decrypt_text(text, shift)}")
+    else:
+        print("Wrong choice")
+
+
+if __name__ == "__main__":
+    main()
